d664230f7196995fe7e8fd662e783c2d2f6f4a4e
DevLeleco <73676777+DevLeleco@users.noreply.github.com>
2021-02-18 11:53:54 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('radar.cliente');
+    return view('dashboard');
 });
 
 /********************************************* Con_Users*********************************************/
