d633a9e04e57fa407ca2e972999a8728b8b4e9af
DevLeleco <73676777+DevLeleco@users.noreply.github.com>
2021-02-18 10:11:44 -0300

    Update cliente.blade.php

--- resources/views/radar/cliente.blade.php
+++ resources/views/radar/cliente.blade.php
@@ -9,6 +9,10 @@
 
 @section('content')
 
+<script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
 <script src=" {{ asset('vendor/jquery/jquery.js') }}"></script>
 <script src=" {{ asset('vendor/sweet/sweetalert2@10.js') }}"></script>
 
