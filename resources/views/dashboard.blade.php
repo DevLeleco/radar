cef42c393d103147de6422ae76570ae9e3cd53cc
DevLeleco <73676777+DevLeleco@users.noreply.github.com>
2021-02-18 12:06:31 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,26 +3,12 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap');
 </style>
-<style>
-  .modal .modal-dialog {width: 30%}
-  </style>
 
 @section('content')
 
 <script src=" {{ asset('vendor/jquery/jquery.js') }}"></script>
-<script src=" {{ asset('vendor/sweet/sweetalert2@10.js') }}"></script>
 
-<script>
-  @if(session('Alert'))
-  swal.fire('{{ session('Alert') }}')
-  @endif
-  @if(session('Danger'))
-  swal.fire('{{ session('Danger') }}')
-  @endif
-  @if(session('Reactivate'))
-  swal.fire('{{ session('Reactivate') }}')
-  @endif
-</script>
+
 
 
 <div class="jumbotron py-1">
